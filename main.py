40f21f2c90247f8dd3d9bf7391aa4a26e56597e9
Yuriy Dmitrievtsev <71283936+YuraDmitrievtsev@users.noreply.github.com>
2020-12-04 20:19:27 +0300

    Причесанная игра

--- main.py
+++ main.py
@@ -1,1 +1,45 @@
 #основная программа
+from os import path
+import random
+import pygame
+from Graphic_objects import *
+
+Field_size = 10
+CELL_SIZE = 50
+
+WIDTH = CELL_SIZE * Field_size
+HEIGHT = CELL_SIZE * Field_size
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH+200, HEIGHT+200))
+screen.fill(WHITE)
+clock = pygame.time.Clock()
+
+
+snd_dir = path.join(path.dirname(__file__),'snd')
+
+move_sound = pygame.mixer.Sound(path.join(snd_dir, 'blip.wav'))
+pygame.mixer.music.load(path.join(snd_dir, 'regression_cyclone.ogg'))
+pygame.mixer.music.set_volume(0.4)
+
+all_sprites = pygame.sprite.Group()
+s = Window(10, 10, 50, move_sound, 100, 100)
+all_sprites.add(s)
+
+pygame.mixer.music.play(loops=-1)
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONUP:
+            all_sprites.update(event)
+
+    all_sprites.update()
+    screen.fill(WHITE)
+    s.draw_score(screen)
+    s.fall()
+    all_sprites.draw(screen)
+    pygame.display.flip()
